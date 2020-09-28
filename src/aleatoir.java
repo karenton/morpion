370775b353f694713cf5e8649c2797bab8412065
jean jacques karenton <karenton23@gmail.com>
2020-09-28 11:57:11 +0200

    NombreAleatoire2

---
+++ src/aleatoir.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+class NombreAleatoire2 {
+    public static void main(String[] args){
+        // génération d'un nombre >= 0 et < 1000
+        Random r = new Random();
+        int n = r.nextInt(1000);
+        System.out.println(n);
+        System.out.println("String choisissez_un_nombre, String tapez_votre_nombre_ici");
+
+import java.util.Scanner;
+
+
+
+        Scanner saisieUtilisateur = new Scanner(System.in);
+
+       ( int, float, double, string , char,...) en fonction du type :
+
+        System.out.println("Veuillez saisir un mot :");
+        String str = saisieUtilisateur.next();
+
+        System.out.println("Veuillez saisir un entier :");
+        int ent = saisieUtilisateur.nextInt();
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
